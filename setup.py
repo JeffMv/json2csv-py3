9be33796d0ae4733c66cb2598fa03c4c92ca90fd
Jeffrey Mvutu Mabilama <JeffMv@users.noreply.github.com>
2020-10-04 22:35:25 +0200

    Updated setup.py to include credits

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ EXTRAS = {
 
 setup(
         name='json2csv',
-        version='0.1',
+        version='0.2',
         modules=['json2csv'],
         scripts=['json2csv.py'],
         url='https://github.com/evidens/json2csv',
         license='MIT License',
         author='evidens',
         author_email='',
-        description='Converts JSON files to CSV (pulling data from nested structures). Useful for Mongo data',
+        description='Converts JSON files to CSV (pulling data from nested structures). Useful for Mongo data. Original author: evidens. New features by @JeffMv (GitHub). @JeffreyMvutu (Twitter)',
         install_requires= [str(ir.req) for ir in parse_requirements('requirements.txt', session=uuid.uuid1())],
         extras_require=EXTRAS,
 )
